818f7c29c8afb43ca9232a1dbd5ac7637cf217ac
Caleb Bardsley <calebgbardsley@gmail.com>
2020-09-24 09:10:17 -0600

    PLANES!!
    I put a Plane in

---
+++ prototype 1 Driving Sym/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX.cs : MonoBehaviour 
+{
+    public GameObject Propeller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
