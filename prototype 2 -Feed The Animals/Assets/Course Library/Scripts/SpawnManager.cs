a906d90a061d6b32573b6f2bbc2c4a74b8b7607e
Caleb Bardsley <calebgbardsley@gmail.com>
2020-10-14 09:10:37 -0600

    Animal Spawn
    Randomized Animal Spawn

--- prototype 2 -Feed The Animals/Assets/Course Library/Scripts/SpawnManager.cs
+++ prototype 2 -Feed The Animals/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    public int animalIndex;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20; 
 
     // Update is called once per frame
     void Update()
     {
-        if(input.GetKeyDown(KeyCode.S))      
+        if(Input.GetKeyDown(KeyCode.S))      
         {
-            Instatiate(animalPrefabs[animalIndex], new Vetor3(0,0,-20), animalPrefabs[animalIndex].transform.rotation);
+            //Randomly Generate Animal Index And Spawn Position
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0 , -spawnPosZ);
+
+            //Spawn Animal Based Off Of Random Number
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }  
     }
 }
