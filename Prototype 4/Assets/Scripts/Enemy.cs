20ab271738e2f9ef9ebb4cb0f99966f5dd4a794f
Caleb Bardsley <calebgbardsley@gmail.com>
2021-01-04 08:21:57 -0700

    ???
    i dont know what this does

--- Prototype 4/Assets/Scripts/Enemy.cs
+++ Prototype 4/Assets/Scripts/Enemy.cs
@@ -7,11 +7,13 @@ public class Enemy : MonoBehaviour
     public float speed = 5;
     private Rigidbody enemyRb;
     private GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         enemyRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
+
     }
 
     // Update is called once per frame
@@ -19,5 +21,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -7,10 +7,12 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemyPrefab;
     private float spawnRange = 9;
 
+    public int enemyCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemyWave(3);
+    
         
         Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
@@ -25,6 +27,18 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0) 
+        {
+            SpawnEnemyWave(1);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i <enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
